e8a2a5b0ec9c303236e1ef1ddcc61e298ce090ad
firdaurus <firda.amalia.lia@gmail.com>
2021-08-31 12:01:57 +0700

    Merge

--- resources/js/app/routes/guest.js
+++ resources/js/app/routes/guest.js
@@ -1,11 +1,8 @@
 import LandingPage from "../pages/public/LandingPage";
 import Login from '../pages/public/Login';
 import RegisterTerapist from '../pages/public/RegisterTerapist'
-<<<<<<< HEAD
 import WhatsNew from "../pages/public/WhatsNew";
-=======
 import FAQ from "../pages/public/FAQ";
->>>>>>> 2643c6427020f13cbac568f31747fa20be208a01
 
 const routes = [
     {
@@ -21,13 +18,11 @@ const routes = [
         component: RegisterTerapist,
     },
     {
-<<<<<<< HEAD
         path: '/apayangbaru',
         component: WhatsNew,
-=======
+    },{
         path: '/faq',
         component: FAQ,
->>>>>>> 2643c6427020f13cbac568f31747fa20be208a01
     },
 ]
 
