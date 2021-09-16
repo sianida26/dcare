c6836ddba20ece1b63287a20161789a31e8f64af
sianida26 <chesakeselekkebo@gmail.com>
2021-09-16 18:11:23 +0700

    Edit Offline

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,7 +1,26 @@
-{{-- @extends('layouts.app') --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Disability Care</title>
+    <link rel="stylesheet" href="{{mix('/css/tailwind.css')}}">
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+    <link rel="stylesheet" href="{{mix('/css/bootstrap-icons.css')}}">
+    <link rel="stylesheet" href="{{mix('/css/fonts/poppins.css')}}">
 
-{{-- @section('content') --}}
+    <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+        integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+        crossorigin=""
+    />
 
-    <h1>You are currently not connected to any networks.</h1>
-
-{{-- @endsection --}}
+    @laravelPWA
+</head>
+<body>
+    <div id="root"></div>
+    <script src="{{mix('/js/index.js')}}"></script>
+</body>
+</html>
